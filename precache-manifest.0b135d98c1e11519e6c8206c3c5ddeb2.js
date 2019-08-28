337c8bf62ff0fb2ada1fb171b15032e33a1c51ec
alessandrokonrad <akonrad6@gmail.com>
2019-08-28 23:11:52 +0200

    Updates

---
+++ precache-manifest.0b135d98c1e11519e6c8206c3c5ddeb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f365c0576570c67ff1f07364069a785",
+    "url": "/ethereum-funding/index.html"
+  },
+  {
+    "revision": "050f60be76deb75069fb",
+    "url": "/ethereum-funding/static/css/main.64f8484d.chunk.css"
+  },
+  {
+    "revision": "2ef4331c6af15cb9b452",
+    "url": "/ethereum-funding/static/js/2.66c1e6e8.chunk.js"
+  },
+  {
+    "revision": "050f60be76deb75069fb",
+    "url": "/ethereum-funding/static/js/main.22a51a16.chunk.js"
+  },
+  {
+    "revision": "85bc558423e43dc922c6",
+    "url": "/ethereum-funding/static/js/runtime~main.2b4d4102.js"
+  }
+]);
